509b3985292a2abe714d2b54e97b6faae38f4f53
Melvin <melvinb9@gmail.com>
2014-06-23 18:54:22 -0700

    Update main.cpp

--- Tic-Tac-Toe/main.cpp
+++ Tic-Tac-Toe/main.cpp
@@ -1,52 +1,84 @@
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
+#include <cstdio>
 
-//beta tic tac toe
+using namespace sf;
 
 int main()
 {
-    // Create the main window
-    sf::RenderWindow window(sf::VideoMode(900, 900), "Tic Tac Toe");
 
-    // Load a sprite to display
-    sf::Texture texture;
-    if (!texture.loadFromFile("tictactoe.png"))
+    sf::RenderWindow window(sf::VideoMode(900, 900), "Tic Tac Toe");     // Create the main window - window.isOpen is now set to True
+    sf::Texture gameboard;      //loads the gameboard image
+    sf::Font font;              //font
+    sf::Music music;            // Load a music to play
+
+    //loads all files
+    if (!gameboard.loadFromFile("tictactoe.png"))
         return EXIT_FAILURE;
-
-    //puts background
-    sf::Sprite sprite(texture);
-    // Create a graphical text to display
-   sf::Font font;
     if (!font.loadFromFile("arial.ttf"))
         return EXIT_FAILURE;
-    sf::Text text("Hello SFML", font, 50);
-    /*
-    // Load a music to play
-    sf::Music music;
     if (!music.openFromFile("swapj.ogg"))
         return EXIT_FAILURE;
-    // Play the music
-        music.play();
-    */
+
+    //renders the use of the files
+    sf::Sprite background(gameboard);       //puts background in window
+    music.play();
+
     // Start the game loop
     while (window.isOpen())
     {
         // Process events
         sf::Event event;
+
         while (window.pollEvent(event))
         {
             // Close window : exit
+            window.display();
             if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape))
                 window.close();
-        }
-        // Clear screen
-        window.clear();
-        // Draw the sprite aka background
-        window.draw(sprite);
-        // Draw the string
+            switch(event.key.code){
+                case '1':
+                    window.close();
+                    break;
+                case '2':
+                    window.close();
+                    break;
+                case '3':
+                    window.close();
+                    break;
+                case '4':
+                    window.close();
+                    break;
+                case '5':
+                    window.close();
+                    break;
+                case '6':
+                    window.close();
+                    break;
+                case '7':
+                    window.close();
+                    break;
+                case '8':
+                    window.close();
+                    break;
+                case '9':
+                    window.close();
+                    break;
+                default:
+                    break;
+            }
 
-        // Update the window
-        window.display();
+
+        }
+
+
+
+
+
+    // Draw the sprite aka background
+    window.draw(background);
+    window.display();  // Update the window
     }
-    return EXIT_SUCCESS;
+    return 0;
 }
+
